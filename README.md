4d6e208923a45af1ada96d63b12d7832fba999c7
Sumit Mittal <sumitmittal.adonis@gmail.com>
2022-06-16 19:23:06 -0400

    Validation rules modified

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
+## Validation Rules - 
+1. Addition and Multiplication can have more than 2 argument<br />
+2. Subtraction and Division should have only 2 arguments<br />
+3. In Division, 2nd number can not be 0<br />
+
+
 ### Addition EndPoint : 
-POST URL  : localhost:8081/calculator/add
+POST URL  : localhost:8081/calculator/add<br />
 JSON Body : [123.1,789.1]
 
 ### Subtraction EndPoint : 
-POST URL  : localhost:8081/calculator/subtract
+POST URL  : localhost:8081/calculator/subtract<br />
 JSON Body : [123.1,789.1]
 
 ### Multiplication EndPoint : 
-POST URL  : localhost:8081/calculator/multiply
+POST URL  : localhost:8081/calculator/multiply<br />
 JSON Body : [123.1,789.1]
 
 ### Division EndPoint : 
-POST URL  : localhost:8081/calculator/divide
+POST URL  : localhost:8081/calculator/divide<br />
 JSON Body : [123.1,789.1]
--- src/main/java/com/sumit/api/utils/InputValidator.java
+++ src/main/java/com/sumit/api/utils/InputValidator.java
@@ -8,11 +8,18 @@ import java.util.List;
 @Component
 public class InputValidator {
 
+    /*
+    Addition and Multiplication can have more than 2 argument
+    Subtraction and Division should have only 2 arguments
+    In Division, 2nd number can not be 0
+     */
     public String validateInputs(OperationType operationType, List<Double> inputs){
         if(inputs == null || inputs.size()<2)
-            return "please provide at least 2 numbers";
-        else if(OperationType.DIVIDE == operationType && inputs.contains(0.0D))
-            return "0 is not allowed in division";
+            return "Please provide at least 2 numbers !!";
+        else if((OperationType.SUBTRACT == operationType || OperationType.DIVIDE == operationType) && inputs.size()>2)
+            return "Input numbers can not be more than 2 !!";
+        else if(OperationType.DIVIDE == operationType && inputs.get(1)==0.0)
+            return "In division 2nd number can not be 0 !!";
         else
             return null;
     }
